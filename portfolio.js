05a3761936a70b46f0b2b8d4ff21a8a1694d7f6a
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-05-18 18:01:49 -0700

    everything except dissappearing

--- portfolio.js
+++ portfolio.js
@@ -4,14 +4,33 @@ var aboutMeLink = document.getElementById("aboutSection")
 console.log(aboutSection)
 var pastProjects = document.getElementById("past-projects")
 console.log(pastProjects)
+var pastProjectsLink = document.getElementById("pastSection")
+console.log(pastSection)
 var whatImDoing = document.getElementById("what-im-doing")
 console.log(whatImDoing)
+var whatImDoingLink = document.getElementById("doingSection")
+console.log(doingSection)
+
 function aboutClick () {
+
     aboutMe.style.display = "block";
     pastProjects.style.display = "none"
     whatImDoing.style.display = "none"
-    
-    
 }
+function pastClick () {
+    pastProjects.style.display = "none"
+    pastProjects.style.display = "block";
+   aboutMe.style.display = "none"
+    whatImDoing.style.display = "none"   
+} 
+  
+function doingClick () {
+   whatImDoing.style.display = "block"
+    aboutMe.style.display = "none"
+    pastProjects.style.display = "none"   
+} 
+
 
 aboutMeLink.addEventListener("click", aboutClick)
+pastProjectsLink.addEventListener("click", pastClick) 
+whatImDoingLink.addEventListener("click", doingClick)
